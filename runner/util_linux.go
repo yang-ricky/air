786c879e31b12c505e8c7059dc77c060e4887073
xiantang <zhujingdi1998@gmail.com>
2022-03-13 16:28:31 +0800

    Fix Zombie process in linux

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -10,22 +10,21 @@ import (
 )
 
 func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
-	pid = cmd.Process.Pid
-
-	if e.config.Build.SendInterrupt {
-		// Sending a signal to make it clear to the process that it is time to turn off
-		if err = syscall.Kill(-pid, syscall.SIGINT); err != nil {
-			return
-		}
-		time.Sleep(e.config.Build.KillDelay * time.Millisecond)
-	}
-
 	// https://groups.google.com/g/golang-nuts/c/XoQ3RhFBJl8
 	// only use (p *Process) Kill() will just kill the process, but it won't also the child process in linux
 	pgid, err := syscall.Getpgid(cmd.Process.Pid)
 	if err != nil {
 		return pgid, err
 	}
+
+	if e.config.Build.SendInterrupt {
+		// Sending a signal to make it clear to the process that it is time to turn off
+		if err = syscall.Kill(-pgid, syscall.SIGINT); err != nil {
+			return
+		}
+		time.Sleep(e.config.Build.KillDelay * time.Millisecond)
+	}
+
 	e.mainDebug("got pgid %v", pgid)
 	if err = syscall.Kill(-pgid, syscall.SIGKILL); err != nil {
 		return pgid, err
