6756585b1bd3ed67c654523ac54f13e37344fe3f
xiantang <zhujingdi1998@gmail.com>
2022-03-13 16:28:26 +0800

    Fix Zombie process in linux

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -30,6 +30,11 @@ func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
 	if err = syscall.Kill(-pgid, syscall.SIGKILL); err != nil {
 		return pgid, err
 	}
+	// Wait releases any resources associated with the Process.
+	_, err = cmd.Process.Wait()
+	if err != nil {
+		return pid, err
+	}
 	e.mainDebug("killed process pid %d successed", pid)
 	return
 }
